1ca60023e7bfb07a4de035e74de127ffe0b8b242
m-fiks <fiksd005@umn.edu>
2018-04-25 14:54:55 -0500

    restarted

--- Letter.js
+++ Letter.js
@@ -1,18 +1,18 @@
-function howdy () {
-    console.log('yo');
+function howdy() {
+  console.log('yo');
 }
 
 howdy();
 
-function Letter (word) {
-    this.underscore = function (){
-        const lettersArray =  word.split('');
-        const underScores = lettersArray.map((elem) => '_');
-        return underScores;
-    }
+function Letter(word) {
+  this.underscore = function () {
+    const lettersArray = word.split('');
+    const underScores = lettersArray.map(elem => '_');
+    return underScores;
+  };
 }
 
 const thingy = new Letter('Pinecone');
 console.log(thingy.underscore());
 
-module.exports = howdy;
+// module.exports = howdy;
